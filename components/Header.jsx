ca133d1380f31229470be443473cd13cdcbef0b9
Mila Vetrenko <lucy.vetrenko@gmail.com>
2024-04-19 12:24:33 +0100

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import logo2 from '../logo2.png'; 
+
+import './Header.css'
+export default function Header(){
+    return(
+      <header className="header">
+
+      <div className="logo">
+      <img className="logo" src="./logo2.png" alt="logo" width={500} />
+      </div>
+
+      <div className="spacer"></div>
+
+      <div className="search">
+        <input type="text" placeholder="Search..." />
+      </div>
+
+      <div className="spacer"></div>
+
+      <div className="user-options">
+        <a href="#">Login / Sign Up</a>
+        <span className="separator">|</span>
+        <a href="#">Become a Seller</a>
+      </div>
+      
+      <div className="spacer"></div>
+
+      <div className="basket">
+        <FontAwesomeIcon icon={faShoppingCart} />
+      </div>
+  
+    </header>
+      
+   
+    )
+}
